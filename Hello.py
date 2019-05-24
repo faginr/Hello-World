cc4cd85e013b9ef73b3208250e1f21d3e6bccdeb
Fagin <rfagin@matw.matthewsintl.com>
2019-05-24 13:28:18 -0700

    Simple first code in python.

---
+++ Hello.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for the user's name.
+
+print ("Hello!")
+print ("What is your name?")
+userName = input()
+print ("Nice to meet you, " + userName)
+print ("Your name is " + str(len(userName)) + " letters long.")
+print ("What is your age?")
+userAge = input()
+print("You will be " + str(int(userAge) + 1) + " in one year.")
